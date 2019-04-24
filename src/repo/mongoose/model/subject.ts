e106a1148b960a7de5f88fc7978b11f2854637ad
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-04-24 05:41:54 +0000

    ### Changed - 旧科目コレクションにプロジェクトを結合

--- src/repo/mongoose/model/subject.ts
+++ src/repo/mongoose/model/subject.ts
@@ -4,9 +4,11 @@ const safe = { j: true, w: 'majority', wtimeout: 10000 };
 
 /**
  * 科目スキーマ
+ * @deprecated accountTitlesに移行予定
  */
 const schema = new mongoose.Schema(
     {
+        project: mongoose.SchemaTypes.Mixed,
         subjectClassificationCd: String,
         subjectClassificationName: String,
         subjectCd: String,
@@ -30,6 +32,27 @@ const schema = new mongoose.Schema(
         },
         toJSON: { getters: true },
         toObject: { getters: true }
+    }
+);
+
+schema.index(
+    { createdAt: 1 },
+    { name: 'searchByCreatedAt' }
+);
+schema.index(
+    { updatedAt: 1 },
+    { name: 'searchByUpdatedAt' }
+);
+
+schema.index(
+    {
+        'project.id': 1
+    },
+    {
+        name: 'searchByProjectId',
+        partialFilterExpression: {
+            'project.id': { $exists: true }
+        }
     }
 );
 
