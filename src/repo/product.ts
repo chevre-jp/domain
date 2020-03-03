1e85fae33b59319bb0d11a4559013631ca375315
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-03-03 09:24:27 +0000

    ### Changed - プロダクトリポジトリを拡張

--- src/repo/product.ts
+++ src/repo/product.ts
@@ -14,6 +14,33 @@ export class MongoRepository {
         this.productModel = connection.model(modelName);
     }
 
+    public static CREATE_MONGO_CONDITIONS(params: any) {
+        // MongoDB検索条件
+        const andConditions: any[] = [];
+
+        const projectIdEq = params.project?.id?.$eq;
+        if (typeof projectIdEq === 'string') {
+            andConditions.push({
+                'project.id': {
+                    $exists: true,
+                    $eq: projectIdEq
+                }
+            });
+        }
+
+        const typeOfEq = params.typeOf?.$eq;
+        if (typeof typeOfEq === 'string') {
+            andConditions.push({
+                typeOf: {
+                    $exists: true,
+                    $eq: typeOfEq
+                }
+            });
+        }
+
+        return andConditions;
+    }
+
     public async findById(params: {
         id: string;
     }): Promise<any> {
@@ -33,5 +60,43 @@ export class MongoRepository {
         }
 
         return doc.toObject();
+    }
+
+    public async search(
+        params: any
+    ): Promise<any[]> {
+        const conditions = MongoRepository.CREATE_MONGO_CONDITIONS(params);
+        const query = this.productModel.find(
+            (conditions.length > 0) ? { $and: conditions } : {},
+            {
+                __v: 0,
+                createdAt: 0,
+                updatedAt: 0
+            }
+        );
+
+        // tslint:disable-next-line:no-single-line-block-comment
+        /* istanbul ignore else */
+        if (params.limit !== undefined && params.page !== undefined) {
+            query.limit(params.limit)
+                .skip(params.limit * (params.page - 1));
+        }
+
+        // tslint:disable-next-line:no-single-line-block-comment
+        /* istanbul ignore else */
+        if (params.sort !== undefined) {
+            query.sort(params.sort);
+        }
+
+        return query.setOptions({ maxTimeMS: 10000 })
+            .exec()
+            .then((docs) => docs.map((doc) => doc.toObject()));
+    }
+
+    public async deleteById(params: {
+        id: string;
+    }) {
+        await this.productModel.findOneAndDelete({ _id: params.id })
+            .exec();
     }
 }
