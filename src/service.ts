41b300a913356e5664d72222b4eec1f9137016d8
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-06-28 00:11:53 +0000

    ### Removed - メンバーシップ登録取引を削除

--- src/service.ts
+++ src/service.ts
@@ -9,7 +9,6 @@ import * as ReportService from './service/report';
 import * as TaskService from './service/task';
 import * as CancelReservationTransactionService from './service/transaction/cancelReservation';
 import * as MoneyTransferTransactionService from './service/transaction/moneyTransfer';
-import * as RegisterProgramMembershipTransactionService from './service/transaction/registerProgramMembership';
 import * as RegisterServiceTransactionService from './service/transaction/registerService';
 import * as ReserveTransactionService from './service/transaction/reserve';
 
@@ -22,7 +21,6 @@ export import task = TaskService;
 export namespace transaction {
     export import cancelReservation = CancelReservationTransactionService;
     export import moneyTransfer = MoneyTransferTransactionService;
-    export import registerProgramMembership = RegisterProgramMembershipTransactionService;
     export import registerService = RegisterServiceTransactionService;
     export import reserve = ReserveTransactionService;
 }
