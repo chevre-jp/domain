53acac900e32c71ec4886826671c0fb672811b09
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2021-04-16 05:43:59 +0000

    - コードリポジトリを追加 - 所有権リポジトリを追加 - プロジェクトメンバーリポジトリを追加 - IAMロールリポジトリを追加

--- src/repository.ts
+++ src/repository.ts
@@ -6,13 +6,16 @@ import { MongoRepository as AccountingReportRepo } from './repo/accountingReport
 import { MongoRepository as AccountTitleRepo } from './repo/accountTitle';
 import { MongoRepository as ActionRepo } from './repo/action';
 import { MongoRepository as CategoryCodeRepo } from './repo/categoryCode';
+import { MongoRepository as CodeRepo } from './repo/code';
 import { MongoRepository as CreativeWorkRepo } from './repo/creativeWork';
 import { MongoRepository as EventRepo } from './repo/event';
 import { MongoRepository as InvoiceRepo } from './repo/invoice';
 import { RedisRepository as ScreeningEventItemAvailabilityRepo } from './repo/itemAvailability/screeningEvent';
+import { MongoRepository as MemberRepo } from './repo/member';
 import { MongoRepository as OfferRepo } from './repo/offer';
 import { MongoRepository as OfferCatalogRepo } from './repo/offerCatalog';
 import { MongoRepository as OrderRepo } from './repo/order';
+import { MongoRepository as OwnershipInfoRepo } from './repo/ownershipInfo';
 import { MongoRepository as PaymentMethodRepo } from './repo/paymentMethod';
 import { MongoRepository as PlaceRepo } from './repo/place';
 import { MongoRepository as PriceSpecificationRepo } from './repo/priceSpecification';
@@ -22,6 +25,7 @@ import { MongoRepository as ProjectRepo } from './repo/project';
 import { RedisRepository as OfferRateLimitRepo } from './repo/rateLimit/offer';
 import { MongoRepository as ReportRepo } from './repo/report';
 import { MongoRepository as ReservationRepo } from './repo/reservation';
+import { MongoRepository as RoleRepo } from './repo/role';
 import { MongoRepository as SellerRepo } from './repo/seller';
 import { MongoRepository as ServiceOutputRepo } from './repo/serviceOutput';
 import { RedisRepository as ServiceOutputIdentifierRepo } from './repo/serviceOutputIdentifier';
@@ -36,18 +40,30 @@ export class AccountingReport extends AccountingReportRepo { }
 export class AccountTitle extends AccountTitleRepo { }
 export class Action extends ActionRepo { }
 export class CategoryCode extends CategoryCodeRepo { }
+/**
+ * 所有権コードリポジトリ
+ */
+export class Code extends CodeRepo { }
 export class CreativeWork extends CreativeWorkRepo { }
 export class Event extends EventRepo { }
 /**
  * 請求書リポジトリ
  */
 export class Invoice extends InvoiceRepo { }
+/**
+ * プロジェクトメンバーリポジトリ
+ */
+export class Member extends MemberRepo { }
 export class Offer extends OfferRepo { }
 export class OfferCatalog extends OfferCatalogRepo { }
 /**
  * 注文リポジトリ
  */
 export class Order extends OrderRepo { }
+/**
+ * 所有権リポジトリ
+ */
+export class OwnershipInfo extends OwnershipInfoRepo { }
 /**
  * 決済方法リポジトリ
  */
@@ -62,6 +78,10 @@ export class Project extends ProjectRepo { }
  */
 export class Report extends ReportRepo { }
 export class Reservation extends ReservationRepo { }
+/**
+ * ロールリポジトリ
+ */
+export class Role extends RoleRepo { }
 /**
  * 販売者リポジトリ
  */
