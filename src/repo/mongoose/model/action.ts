0662c8fe015c620954340771a3d170edd1e0a083
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2021-04-26 02:08:11 +0000

    - cinerinoのMongoDBインデックスを移行

--- src/repo/mongoose/model/action.ts
+++ src/repo/mongoose/model/action.ts
@@ -31,7 +31,7 @@ const schema = new mongoose.Schema(
         id: true,
         read: 'primaryPreferred',
         writeConcern: writeConcern,
-        strict: false,
+        strict: true,
         useNestedStrict: true,
         timestamps: {
             createdAt: 'createdAt',
@@ -107,6 +107,26 @@ schema.index(
 );
 
 schema.index(
+    { 'agent.typeOf': 1, startDate: -1 },
+    {
+        name: 'searchByAgentTypeOf',
+        partialFilterExpression: {
+            'agent.typeOf': { $exists: true }
+        }
+    }
+);
+
+schema.index(
+    { 'agent.id': 1, startDate: -1 },
+    {
+        name: 'searchByAgentId',
+        partialFilterExpression: {
+            'agent.id': { $exists: true }
+        }
+    }
+);
+
+schema.index(
     { 'purpose.typeOf': 1, startDate: -1 },
     {
         name: 'searchByPurposeTypeOf-v2',
@@ -177,6 +197,66 @@ schema.index(
 );
 
 schema.index(
+    { 'object.event.id': 1, startDate: -1 },
+    {
+        name: 'searchByObjectEventId',
+        partialFilterExpression: {
+            'object.event.id': { $exists: true }
+        }
+    }
+);
+
+schema.index(
+    { 'object.acceptedOffer.ticketedSeat.seatNumber': 1, startDate: -1 },
+    {
+        name: 'searchByObjectAcceptedOfferTicketedSeatSeatNumber',
+        partialFilterExpression: {
+            'object.acceptedOffer.ticketedSeat.seatNumber': { $exists: true }
+        }
+    }
+);
+
+schema.index(
+    { 'object.orderNumber': 1, startDate: -1 },
+    {
+        name: 'searchByObjectOrderNumber-v2',
+        partialFilterExpression: {
+            'object.orderNumber': { $exists: true }
+        }
+    }
+);
+
+schema.index(
+    { 'purpose.orderNumber': 1, startDate: -1 },
+    {
+        name: 'searchByPurposeOrderNumber-v2',
+        partialFilterExpression: {
+            'purpose.orderNumber': { $exists: true }
+        }
+    }
+);
+
+// schema.index(
+//     { 'object.paymentMethod': 1, startDate: -1 },
+//     {
+//         name: 'searchByObjectPaymentMethod',
+//         partialFilterExpression: {
+//             'object.paymentMethod': { $exists: true }
+//         }
+//     }
+// );
+
+// schema.index(
+//     { 'object.paymentMethodId': 1, startDate: -1 },
+//     {
+//         name: 'searchByObjectPaymentMethodId',
+//         partialFilterExpression: {
+//             'object.paymentMethodId': { $exists: true }
+//         }
+//     }
+// );
+
+schema.index(
     { 'object.reservationFor.id': 1, startDate: -1 },
     {
         name: 'searchByObjectReservationForId',
@@ -202,6 +282,86 @@ schema.index(
         name: 'searchByResultId',
         partialFilterExpression: {
             'result.id': { $exists: true }
+        }
+    }
+);
+
+schema.index(
+    { 'result.code': 1, startDate: -1 },
+    {
+        name: 'searchByResultCode',
+        partialFilterExpression: {
+            'result.code': { $exists: true }
+        }
+    }
+);
+
+schema.index(
+    { 'result.orderNumber': 1, startDate: -1 },
+    {
+        name: 'searchByResultOrderNumber',
+        partialFilterExpression: {
+            'result.orderNumber': { $exists: true }
+        }
+    }
+);
+
+schema.index(
+    { 'fromLocation.typeOf': 1, startDate: -1 },
+    {
+        name: 'searchByFromLocationTypeOf',
+        partialFilterExpression: {
+            'fromLocation.typeOf': { $exists: true }
+        }
+    }
+);
+
+schema.index(
+    { 'fromLocation.accountNumber': 1, startDate: -1 },
+    {
+        name: 'searchByFromLocationAccountNumber',
+        partialFilterExpression: {
+            'fromLocation.accountNumber': { $exists: true }
+        }
+    }
+);
+
+schema.index(
+    { 'fromLocation.accountType': 1, startDate: -1 },
+    {
+        name: 'searchByFromLocationAccountType',
+        partialFilterExpression: {
+            'fromLocation.accountType': { $exists: true }
+        }
+    }
+);
+
+schema.index(
+    { 'toLocation.typeOf': 1, startDate: -1 },
+    {
+        name: 'searchByToLocationTypeOf',
+        partialFilterExpression: {
+            'toLocation.typeOf': { $exists: true }
+        }
+    }
+);
+
+schema.index(
+    { 'toLocation.accountNumber': 1, startDate: -1 },
+    {
+        name: 'searchByToLocationAccountNumber',
+        partialFilterExpression: {
+            'toLocation.accountNumber': { $exists: true }
+        }
+    }
+);
+
+schema.index(
+    { 'toLocation.accountType': 1, startDate: -1 },
+    {
+        name: 'searchByToLocationAccountType',
+        partialFilterExpression: {
+            'toLocation.accountType': { $exists: true }
         }
     }
 );
